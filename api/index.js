ec40d9d4f466f4636a751d0c602f2958bdc89f72
nc-devw <nc.devw@gmail.com>
2021-02-11 20:24:40 -0300

    repo init 2

--- api/index.js
+++ api/index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const { Sequelize } = require('sequelize');
 
 app.use(morgan('dev'));
 
-app.listen(4000, (err) => {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log('Escuchando en localhost:4000');
-	}
+const sequelize = new Sequelize(`postgres://postgres:12345@localhost/taller`, {
+	logging: false,
+	native: false,
+});
+
+sequelize.sync({ force: true }).then(() => {
+	app.listen(4000, (err) => {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log('Escuchando en localhost:4000');
+		}
+	});
 });
